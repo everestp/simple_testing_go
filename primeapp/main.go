cca07df7c3874225fdd005892436bd9d3ae8d29c
everestp <paudeleverest09@gmail.com>
2026-01-04 19:22:04 +0545

    15. Improving  program  to allow user entered information

--- primeapp/main.go
+++ primeapp/main.go
@@ -1,32 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-  n :=121
-  _ ,msg :=isPrime(n)
-  fmt.Println(msg)
+// Print a welcome message
+  intro()
+// create a channel to indicate when the use  want to quite
+  doneChan :=make(chan bool)
+
+
+//start a goroutine to read user input and run program
+go readUerInput(doneChan)
+
+// block until the doneChan gets a value
+<-doneChan
+
+//close the channel
+close(doneChan)
+// say goodbuy
+
+fmt.Println("Goodbye. ")
+}
+ func readUerInput(doneChan chan bool){
+	scanner :=bufio.NewScanner(os.Stdin)
+	for {
+		res , done := checkNumbers(scanner)
+		if done {
+			doneChan <- false
+			return
+		}
+
+		fmt.Println(res)
+		promt()
+	}
+ }
+
+func checkNumbers(scanner *bufio.Scanner)(string , bool){
+	// read user input
+scanner.Scan()
+	//check to see if the user want to quite
+	if strings.EqualFold(scanner.Text(), "q"){
+		return "", true
+	}
+	// try to convert what use type into an int
+	numtoCheck , err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Please enter an whole number", false
+	}
+
+	_ ,msg := isPrime(numtoCheck)
+	return msg, false
+	
 }
 
+func intro(){
+	fmt.Println("Is it Prime?")
+	fmt.Println("--------------")
+	fmt.Println("Enter a whole number , and we'll tell you if it is prime or not .Enter q to quite")
+	promt()
+}
+
+func promt(){
+	fmt.Print("->")
+}
 func isPrime(n int) (bool, string) {
+	// 0 and 1 are not prime by definition
 	if n == 0 || n == 1 {
-		return false, fmt.Sprintf("%d is not prime, by definition", n)
-
+		return false, fmt.Sprintf("%d is not prime, by definition!", n)
 	}
+
+	// negative numbers are not prime
 	if n < 0 {
-		return false, fmt.Sprintf("%d is not prime, by definition Negative number are not prime", n)
-
+		return false, "Negative numbers are not prime, by definition!"
 	}
 
-	// use the modulas operator  repeatedly if we have a prime number
-
+	// use the modulus operator repeatedly to see if we have a prime number
 	for i := 2; i <= n/2; i++ {
 		if n%i == 0 {
-			// not a prime  number
-			return false, fmt.Sprintf("%d is not prime, because it is divisible by %d", n, i)
+			// not a prime number
+			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d!", n, i)
 		}
 	}
-  return true, fmt.Sprintf("%d is  prime number !", n)
+
+	return true, fmt.Sprintf("%d is a prime number!", n)
 }
-
-
--- primeapp/main_test.go
+++ primeapp/main_test.go
@@ -1,35 +1,33 @@
 package main
 
-import (
-	
-	"testing"
-)
+import "testing"
 
-
-
-func  Test_isPrime(t *testing.T){
-	primeTests :=[]struct{
+func Test_isPrime(t *testing.T) {
+	primeTests := []struct {
 		name string
 		testNum int
 		expected bool
 		msg string
-
 	}{
-		{"prime" ,7,true ,"7 is  prime number !"},
-	 }
-	for _ , e := range primeTests{
-		result ,msg := isPrime(e.testNum)
-		if e.expected && !result{
-			t.Errorf("%s: expected true but go false", e.name)
+		{"prime", 7, true, "7 is a prime number!"},
+		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative number", -11, false, "Negative numbers are not prime, by definition!"},
+	}
+
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true but got false", e.name)
 		}
 
-		if !e.expected && result{
-		t.Errorf("%s: expected false but go true", e.name)
+		if !e.expected && result {
+			t.Errorf("%s: expected false but got true", e.name)
 		}
 
-		if e.msg != msg{
-			t.Errorf("%s: expected %s but go %s", e.name , e.msg , msg)
+		if e.msg != msg {
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
 		}
 	}
-	
 }
