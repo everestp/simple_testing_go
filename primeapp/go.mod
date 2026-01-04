cb2dc99375468ae76b51a268378e26c2db142907
everestp <paudeleverest09@gmail.com>
2026-01-04 17:27:11 +0545

    Simple prime test

---
+++ primeapp/go.mod
@@ -0,0 +1,3 @@
+module github.com/everestp/primeapp
+
+go 1.24.11
---
+++ primeapp/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+  n :=121
+  _ ,msg :=isPrime(n)
+  fmt.Println(msg)
+}
+
+func isPrime(n int) (bool, string) {
+	if n == 0 || n == 1 {
+		return false, fmt.Sprintf("%d is not prime, by definition", n)
+
+	}
+	if n < 0 {
+		return false, fmt.Sprintf("%d is not prime, by definition Negative number are not prime", n)
+
+	}
+
+	// use the modulas operator  repeatedly if we have a prime number
+
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			// not a prime  number
+			return false, fmt.Sprintf("%d is not prime, because it is divisible by %d", n, i)
+		}
+	}
+  return true, fmt.Sprintf("%d is  prime number !", n)
+}
